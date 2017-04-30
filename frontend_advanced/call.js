904a5878abb79aed9ab1ce6eff97aeb6675843aa
Xinyang <xinyzhang9@gmail.com>
2017-04-29 23:35:44 -0700

    call

---
+++ frontend_advanced/call.js
@@ -0,0 +1,16 @@
+var animals = [
+  { species: 'Lion', name: 'King' },
+  { species: 'Whale', name: 'Fail' }
+];
+
+for (var i = 0; i < animals.length; i++) {
+  (function(i) {
+    this.print = function() {
+      console.log('#' + i + ' ' + this.species
+                  + ': ' + this.name);
+    }
+    this.print();
+  }).call(animals[i], i);
+}
+
+// animals[i] is the context of each function call
